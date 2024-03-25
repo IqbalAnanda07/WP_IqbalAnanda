013c207b352d1a447577b609401d15dffaadfe72
IqbalAnanda07 <iqbalananda1522@gmail.com>
2024-03-25 09:54:47 +0700

    tugas 1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,9 +1,12 @@
 <?php
-class Contoh1 extends index()
+class Contoh1 extends CI_Controller
 {
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
         echo"Nama saya Iqbal Ananda
+            Saya tinggal di daerah Serdam
+            olahraga yang saya sukai adalah
+            Basket";
     }
 }
