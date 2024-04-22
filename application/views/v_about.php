6f3957c3351d5a54c3137323fb14a9146a0fed8a
IqbalAnanda07 <iqbalananda1522@gmail.com>
2024-04-22 10:02:06 +0700

    Pertemuan 4 dan Tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Iqbal</li>
+        <li>Nama Belakang : Ananda</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="circle">
+        <li> Jln.Sungai Raya Dalam</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kisaran</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Basket</li>
+        <li>Sepak Bola</li>
+    </ul>
+</section>
