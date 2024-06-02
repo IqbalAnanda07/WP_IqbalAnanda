62b2149a4bf752408c5be4184cf37502d171fcec
IqbalAnanda07 <iqbalananda1522@gmail.com>
2024-06-02 18:58:59 +0700

    pertemuan 10 dan tugas 8

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -1,16 +1,12 @@
 <html>
-
-<head> 
-<title>Tampil Data Matakuliah</title>
+<head>
+    <title>Tampil Data Matakuliah</title>
 </head>
-
 <body>
     <center>
         <table>
             <tr>
-                <th colspan="3">
-                    Tampil Data Mata Kuliah
-                </th>
+                <th colspan="3">Tampil Data Mata Kuliah</th>
             </tr>
             <tr>
                 <td colspan="3">
@@ -20,32 +16,22 @@
             <tr>
                 <th>Kode MTK</th>
                 <th>:</th>
-                <td>
-                    <?= $kode; ?>
-                </td>
+                    <td><?= $kode; ?></td>
             </tr>
             <tr>
-                <td>Nama MTK</td>
-                <td>:</td>
-                <td>
-                    <?= $nama; ?>
-                </td>
+                <th>Nama MTK</th>
+                <th>:</th>
+                    <td><?= $nama; ?></td>
             </tr>
             <tr>
-                <td>SKS</td>
-                <td>:</td>
-                <td>
-                    <?= $sks; ?>
-                </td>
+                <th>SKS</th>
+                <th>:</th>
+                    <td><?= $sks; ?></td>
             </tr>
             <tr>
-                <td colspan="3" align="center">
-                    <a href="<?= base_url('matakuliah');
-?>">Kembali</a>
-                </td>
-                </tr>
+                <td colspan="3" align="center"><a href="<?= base_url('matakuliah');?>">Kembali</a></td>
+            </tr>
         </table>
     </center>
 </body>
-
-</html> 
+</html>
